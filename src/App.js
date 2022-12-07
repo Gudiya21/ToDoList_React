752a97aab2dd39b23c7e5ba0481636b60c3edff1
Gudia <91308789+Gudiya21@users.noreply.github.com>
2022-12-06 23:14:44 -0800

    todo list in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React,{ useState } from 'react';
+import './App.css'
+function App(){
+const [task,setTask]=useState("")
+const [alltasks,setAllTasks]=useState([])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <center>
+      <h1 id='headname'>TODO LIST APP</h1>
+    <div id='container'>
+      <input id='inputbox' type="text" placeholder='Enter Your Task' onChange={(element)=>{
+        setTask(element.target.value)
+      }}/>
+    <button id='add' onClick={()=>{
+      if(task.length>0){
+      setAllTasks([...alltasks,task])
+      }
+    }}>Add ToDo</button>
+    {
+      alltasks.map((item,index)=>{
+        return(
+          <div>
+            <h3>{item}</h3>
 
-export default App;
+            <button id='delete' onClick={()=>{
+
+              let storeData=[...alltasks]
+              storeData.splice(index,1); 
+              setAllTasks(storeData)
+            }}>Delete</button>
+             
+            <button id='edit' onClick={()=>{
+              let newArr=[...alltasks];
+              let editvalue = prompt("enter the edited todo")
+              newArr[index]=editvalue
+              setAllTasks(newArr)
+            }}>EDIT</button>
+            <button id='complete' onClick={()=>{
+              let newArr = [...alltasks];
+              newArr[index]=<strike>{newArr[index]}</strike>
+              setAllTasks(newArr)    
+            }}>Complete</button>
+          </div>
+        )
+      })
+    }
+    </div>
+    </center>
+  )
+}
+export default App
+
+
+
